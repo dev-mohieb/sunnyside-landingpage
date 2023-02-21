4e162fb1785381b2ee8adead076764f427d9cd9f
Mohieb <dev.mohieb@gmail.com>
2023-02-21 18:03:22 +0300

    add active effect for a tags, add nav toggle effect

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const burgerBtn = document.querySelector('#burger');
 const navMenu = document.querySelector('#nav-menu');
 const navChildren = navMenu.children;
 
+const learnMoreYellow = document.querySelector('.learn-more-yellow')
+const learnMoreRed = document.querySelector('.learn-more-red')
+
+
 burgerBtn.addEventListener('click', () => {
     if (navMenu.classList.contains('hide')) {
         navMenu.classList.replace('hide', 'show');
@@ -22,3 +26,17 @@ for (let child of navChildren) {
         navMenu.classList.replace('show','hide');
     })
 }
+
+learnMoreRed.addEventListener('mouseover', () => {
+    learnMoreRed.previousElementSibling.classList.replace('bg-softRedFEM/40', 'bg-softRedFEM/100')
+})
+learnMoreRed.addEventListener('mouseout', () => {
+    learnMoreRed.previousElementSibling.classList.replace('bg-softRedFEM/100', 'bg-softRedFEM/40')
+})
+
+learnMoreYellow.addEventListener('mouseover', () => {
+    learnMoreYellow.previousElementSibling.classList.replace('bg-yellowFEM/40', 'bg-yellowFEM/100')
+})
+learnMoreYellow.addEventListener('mouseout', () => {
+    learnMoreYellow.previousElementSibling.classList.replace('bg-yellowFEM/100', 'bg-yellowFEM/40')
+})
